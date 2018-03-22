ad7d7866e18186f2faa8ff7eeff7acc01f2404ac
Nagazaki Beck <hackedconta@gmail.com>
2018-03-22 19:18:06 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,38 +8,75 @@ from anotacao import *
 if __name__ == '__main__':
 
 	adao = AnotacaoDAO()
-	cont = 1
+	arq = open("banco.txt", "a+")
+	arq.close()
 	while(1):
-		op = input("Escolha: (1) Adicionar, (2) Listar, (3) Excluir, (4) Ir para lixeira (0) Sair ")
+		op = input("Anotacao: (1) Adicionar, (2) Listar, (3) Excluir, (4) Alterar, (5) Duplicar, (6) Exportar  (7) Ir para lixeira (0) Sair: ")
 		if(op == 1):
+			print("---------------- Adicionando Anotacao ----------------")
 			titulo = raw_input("Titulo: ")
-			data = raw_input("Data (dd/mm/aaaa): ")
+			data = datetime.now().strftime("%d-%m-%y-%H-%M")
 			desc = raw_input("Descricao: ")
-			pk = cont
-			an = Anotacao(titulo,data,desc,pk)
+			a = adao.listar()
+			if(len(a) == 0): pk = 1
+			else:
+				pk = adao.listar()[-1].pk+1
+			an = Anotacao(titulo,desc,str(data),pk)
 			adao.adicionar(an)
+			print(" >> Adicionada com sucesso" + "\n") 
 		elif(op == 2):
+			print("---------------- Listando Anotacao ----------------")
 			vet = adao.listar()
 			for n in vet:
-				print n.titulo + " - " + n.data + " - " + n.descricao + " - " + str(n.pk) +"\n"	
+				print "Titulo: " + n.titulo + "  " + "Descricao: " + n.descricao + "  " + "Codigo: " + str(n.pk) + "  " + "Data: " + str(n.data) +"\n"	
 
 		elif(op == 3):
+			print("---------------- Excluindo Anotacao ----------------")
 			pk = input("Digite o id da anotacao: ")
 			question = raw_input("Excluir permanentemente (s/n): ")
-			if(question == "s"): adao.excluir(pk)
+			if(question == "s"):
+				adao.excluir(pk)
+				print(" >> Excluida com sucesso" + "\n")
 			else:
 				adao.lixeira(pk)
-				print("Anotacao movida para a lixeira")
-		elif (op == 0): break
+				print(" >> Movida para a lixeira" + "\n")
 		elif(op == 4):
+			print("---------------- Alterando Anotacao ----------------")
+			pk = input("Digite o id da anotacao: ")
+			t = raw_input("Novo titulo: ")
+			d = raw_input("Nova descricao: ")
+			a = adao.obter(pk)
+			if(t != ""): a.titulo = t
+			if(d != ""): a.descricao = d
+			adao.alterar(a)
+			print(" >> Alterada com sucesso" + "\n")
+		elif(op == 5):
+			print("---------------- Duplicando Anotacao ----------------")
+			pk = input("Digite o id da anotacao: ")
+			a = adao.obter(pk)
+			adao.duplicar(a)
+			print(" >> Duplicada com sucesso" + "\n")
+		elif(op == 6):
+			print("---------------- Exportando Anotacoes ----------------")
+			adao.exportar()
+			print(" >> Exportadas com sucesso")
+		elif (op == 0): break
+		elif(op == 7):
 			while(1):
-				x = input("Lixeira: (1) Restaurar, (2) Listar, (0) Voltar ")
+				x = input("Lixeira: (1) Restaurar, (2) Listar, (3) Excluir (0) Voltar:  ")
 				if(x == 1):
+					print("---------------- Restaurando Anotacao ----------------")
 					pk = input("Digite o id da anotacao: ")
+					print(" >> Restaurada com sucesso" + "\n")
 					adao.restaurar(pk)
 				if(x == 2 ):
+					print("---------------- Listando Anotacao ----------------")
 					vet = adao.listarLixeira()
-					for aa in vet:
-						print aa.titulo + " - " + aa.data + " - " + aa.descricao + " - " + str(aa.pk) +"\n"	
+					for n in vet:
+						print "Titulo: " + n.titulo + "  " + "Descricao: " + n.descricao + "  " + "Codigo: " + str(n.pk) + "  " + "Data: " + str(n.data) +"\n"	
+				if(x == 3):
+					print("---------------- Deletando Anotacao ----------------")
+					pk = input("Digite o id da anotacao: ")
+					adao.lixeiraLimpa(pk)
+					print(" >> Apagada com sucesso" + "\n")		
 				if(x == 0): break;	
-		cont = cont + cont
--- persistencia.py
+++ persistencia.py
@@ -15,10 +15,10 @@ class AnotacaoDAO:
 		vet = []
 		for linha in arq:
 			aux = linha.strip().split(";")
-			anotacao = Anotacao(aux[0], aux[1], aux[2], int(aux[3]))
+			anotacao = Anotacao(aux[0], aux[1], str(aux[2]), int(aux[3]))
 			vet.append(anotacao)
 		arq.close()
-		return vet
+		return sorted(vet, key=lambda obj: obj.data)
 
 	def adicionar(self, anotacao):
 		arq = open("banco.txt", "a+")
@@ -76,4 +76,46 @@ class AnotacaoDAO:
 			if(a.pk == pk):
 				arq.write(a.obj2CSV())
 		arq.close()
-		self.lixeiraLimpa(pk)		
+		self.lixeiraLimpa(pk)
+
+	def alterar(self, anotacao):
+		vet = self.listar()
+		vetAux = []
+		for a in vet:
+			if (a.pk == anotacao.pk):
+				vetAux.append(anotacao)
+			else:
+				vetAux.append(a)			
+		arquivo = open("banco.txt", "w")
+		arquivo.close()		
+		for e in vetAux: 
+			self.adicionar(e)
+
+	def duplicar(self,anotacao):
+		a = self.obter(anotacao.pk)
+		an = Anotacao(a.titulo,a.descricao,a.data,a.pk)
+		self.adicionar(an)
+
+	def exportar(self):
+		meses = ["","janeiro","fevereiro","marco","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"]
+		qtdePorMes = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+		vet = self.listar()
+
+		for m in meses:
+			arquivo = open(m+".txt","w")
+			arquivo.close()
+			
+		for n in vet:
+			ab = n.data.split("-")
+			mes = int(ab[1])
+			qtdePorMes[mes] = qtdePorMes[mes] + 1
+			arquivo = open(meses[mes]+".txt","a")
+			arquivo.write(n.obj2CSV())
+			arquivo.close()
+
+		i = 0
+		while i < len(qtdePorMes):
+			if (qtdePorMes[i] == 0):
+				os.remove(meses[i]+".txt")
+			i = i + 1	
+		
